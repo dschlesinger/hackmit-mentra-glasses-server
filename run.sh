631c1cc9eab6d77f6c91c5a8907167934e85768e
dschlesinger <denalischlesinger06@gmail.com>
2025-09-14 07:33:55 -0400

    Update run script docs

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-# This does works I think
+# This does works I think, if doesn't copy this into 2 terminals
+# ngrok http --url=minnie-unbeloved-myong.ngrok-free.app 5000
+# bun run dev
 NGROK_URL=minnie-unbeloved-myong.ngrok-free.app
 
 ngrok http --url=${NGROK_URL} 5000 & bun run dev
